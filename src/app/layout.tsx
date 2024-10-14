f63bc1bd395f54c07653aed7271f6cc18b51dbf7
Yashnotfound <yashshokeen161019@gmail.com>
2024-10-14 17:37:09 +0530

    2nd commmit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
-  weight: "100 900",
+  weight: "100 950",
 });
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
