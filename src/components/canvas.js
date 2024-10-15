07397ab184317df8daffb47559809b4e439dcb1e
Limax <limax.workspace@gmail.com>
2024-10-15 17:47:32 +0200

    feat: add transformer

--- src/components/canvas.js
+++ src/components/canvas.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Stage, Layer, Star, Text } from "react-konva";
+import React, { useRef, useEffect } from "react";
+import { Stage, Layer, Star, Text, Transformer } from "react-konva";
 
 function generateShapes() {
   return [...Array(10)].map((_, i) => ({
@@ -7,61 +7,97 @@ function generateShapes() {
     x: Math.random() * window.innerWidth,
     y: Math.random() * window.innerHeight,
     rotation: Math.random() * 180,
+    innerRadius: 20,
+    outerRadius: 40,
+    fill: "yellow",
+    stroke: "black",
+    strokeWidth: 2,
     isDragging: false,
   }));
 }
 
 const INITIAL_STATE = generateShapes();
 
+const StarShape = ({ star, isSelected, onSelect, onChange }) => {
+  const shapeRef = useRef();
+  const trRef = useRef();
+
+  useEffect(() => {
+    if (isSelected) {
+      trRef.current.nodes([shapeRef.current]);
+      trRef.current.getLayer().batchDraw();
+    }
+  }, [isSelected]);
+
+  return (
+    <>
+      <Star
+        ref={shapeRef}
+        {...star}
+        onClick={onSelect}
+        onTap={onSelect}
+        draggable
+        onDragEnd={(e) => {
+          onChange({
+            ...star,
+            x: e.target.x(),
+            y: e.target.y(),
+          });
+        }}
+      />
+      {isSelected && (
+        <Transformer
+          ref={trRef}
+          flipEnabled={false}
+          boundBoxFunc={(oldBox, newBox) => {
+            if (Math.abs(newBox.width) < 5 || Math.abs(newBox.height) < 5) {
+              return oldBox;
+            }
+            return newBox;
+          }}
+        />
+      )}
+    </>
+  );
+};
+
 const Canvas = () => {
   const [stars, setStars] = React.useState(INITIAL_STATE);
+  const [selectedId, setSelectedId] = React.useState(null);
 
-  const handleDragStart = (e) => {
-    const id = e.target.id();
-    setStars(
-      stars.map((star) => {
-        return {
-          ...star,
-          isDragging: star.id === id,
-        };
-      })
-    );
+  const handleSelect = (id) => {
+    setSelectedId(id);
   };
-  const handleDragEnd = () => {
-    setStars(
-      stars.map((star) => ({
-        ...star,
-        isDragging: false,
-      }))
+
+  const handleChange = (newAttrs) => {
+    const updatedStars = stars.map((star) =>
+      star.id === newAttrs.id ? newAttrs : star
     );
+    setStars(updatedStars);
+  };
+
+  const handleDeselect = (e) => {
+    if (e.target === e.target.getStage()) {
+      setSelectedId(null);
+    }
   };
 
   return (
-    <Stage width={window.innerWidth} height={window.innerHeight}>
+    <Stage
+      width={window.innerWidth}
+      height={window.innerHeight}
+      onMouseDown={handleDeselect}
+      onTouchStart={handleDeselect}
+    >
       <Layer>
         <Text text="Try to drag a star" />
         {stars.map((star) => (
-          <Star
+          <StarShape
             key={star.id}
-            id={star.id}
-            x={star.x}
-            y={star.y}
-            numPoints={5}
-            innerRadius={20}
-            outerRadius={40}
-            fill="#89b717"
-            opacity={0.8}
-            draggable
-            rotation={star.rotation}
-            shadowColor="black"
-            shadowBlur={10}
-            shadowOpacity={0.6}
-            shadowOffsetX={star.isDragging ? 10 : 5}
-            shadowOffsetY={star.isDragging ? 10 : 5}
-            scaleX={star.isDragging ? 1.2 : 1}
-            scaleY={star.isDragging ? 1.2 : 1}
-            onDragStart={handleDragStart}
-            onDragEnd={handleDragEnd}
+            star={star}
+            isSelected={star.id === selectedId}
+            onSelect={() => handleSelect(star.id)}
+            onChange={handleChange}
           />
         ))}
       </Layer>
